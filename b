9797d2d1a84a3f931992411ce80d2053a563d92f
Dzamir <Dzamir@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-10 08:51:03 +0000

    database creation
    git-svn-id: https://plugins.svn.wordpress.org/wiki-dashboard/trunk@21339 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ b
@@ -0,0 +1,12 @@
+PROVA 1234 ..
+
+Inserito il nuovo progetto usb autostart!!!!!!!
+Grande andrea!!! Bisogna sistemare la pagina del progetto e dopo che passano un pò di giorni dal post sulla miniwiki inserire il post dove si presenta usbautostart :(
+
+Link da cui prendere una foto interessante per articoli su USB: http://www.vanderlee.com/plug-ins_nightvision.html
+
+Lista cose da fare di Davide:
+* Mangiare la nutella
+* Scrivere un articolo sulle linked list in c++
+* Strutturare le pagine del progetto miniwiki e fare 1 post
+* Fare la nuova versione di miniwiki con i diff
---
+++ wikidashboard-core.php
@@ -0,0 +1,44 @@
+<?php
+
+class WikiDashboard {
+
+	function CheckForInstall() 
+	{
+		global $wpdb;
+		$installed_version = get_option($OPTION_VERSION);
+		if ($installed_version == '') 
+			$installed_version = 0;
+
+		if ($installed_version < 1)
+		{
+				$q = <<<SQL
+					CREATE TABLE  `wordpress`.`wp_wiki_dashboard_content` (
+					  `UserID` bigint(20) NOT NULL COMMENT '0 for public wiki',
+					  `PostID` int(10) unsigned NOT NULL default '0',
+					  `RevisionID` int(11) NOT NULL,
+					  `Title` text NOT NULL,
+					  `Content` longtext,
+					  `ModifiedUserID` bigint(20) NOT NULL COMMENT 'ID Of the user that modified this revision',
+					  `Date` datetime NOT NULL,
+					  PRIMARY KEY  (`UserID`,`PostID`,`RevisionID`)
+					) TYPE=MyISAM;
+SQL;
+
+				$wpdb->query($q);
+
+	//			add_option($OPTION_VERSION, $version, 'Program version', 'yes');
+		}
+
+		if ($installed_version != $current_version) {
+			update_option($OPTION_VERSION, $current_version);
+		}
+	}
+
+	function AddRevision($userID, $postID)
+	{
+		
+	}
+
+}
+
+?>
